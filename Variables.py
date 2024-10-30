f39f8b1b94a95ae9318b892e54c4e65b1963a1f7
xFreezardx <48527278+xFreezardx@users.noreply.github.com>
2024-10-30 22:32:52 +0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,5 @@
+Homework_amount = 12
+Hours_spent = 1.5
+Course_name = 'Python'
+TimePerTask = Homework_amount / Hours_spent
+print('Курс:' , Course_name , ', всего задач:' , Homework_amount , ', затрачено часов:' , Hours_spent , ', среднее время выполнения' , TimePerTask , 'часа.')
